5682c9510c416b841739fa8120f414c3b1fa06b1
Matthew Rizzini <matthewrizzini@gmail.com>
2017-11-18 03:44:19 +0000

    Version 5 adding on displayTodos

--- watch.js
+++ watch.js
@@ -1,7 +1,18 @@
 var todoList = {
   todos: [],
   displayTodos: function () {
-      console.log('My todos:', this.todos);
+      if (this.todos.length === 0) {
+          console.log('Your todo list is empty!');
+      } else {
+      console.log('My todos:');
+      for (var i = 0; i < this.todos.length; i++) {
+          if (this.todos[i].completed === true) {
+            console.log('(x)', this.todos[i].todoText);
+          } else {
+            console.log('( )', this.todos[i].todoText);
+          }
+      }
+     }
   },
   addTodo: function (todoText) {
     this.todos.push({
@@ -26,7 +37,9 @@ var todoList = {
   }
 };
 
-
-todoList.addTodo('1st try');
-todoList.changeTodo(0, '2nd try');
+todoList.addTodo('first');
+todoList.addTodo('second');
+todoList.toggleCompleted(1);
 todoList.toggleCompleted(0);
+todoList.toggleCompleted(1);
+
