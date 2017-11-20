e88eca473b3765cffe513e994f90556e1411cab8
Matthew Rizzini <matthewrizzini@gmail.com>
2017-11-20 18:31:52 +0000

    Version 6 adding Toggle Completed

--- watch.js
+++ watch.js
@@ -34,12 +34,33 @@ var todoList = {
       var todo = this.todos[position];
       todo.completed = !todo.completed;
       this.displayTodos(); 
+  },
+  
+  toggleAll: function () {
+    var totalTodos = this.todos.length;
+    var completedTodos = 0;
+    for (var i = 0; i < totalTodos; i++) {
+      if (this.todos[i].completed === true) {
+        completedTodos++;
+      }
+    }
+    if (completedTodos === totalTodos) {
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = false;
+      }
+    } else {
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = true;
+      }
+    }
+    this.displayTodos();
   }
 };
 
+
 todoList.addTodo('first');
 todoList.addTodo('second');
-todoList.toggleCompleted(1);
+todoList.addTodo('third');
 todoList.toggleCompleted(0);
-todoList.toggleCompleted(1);
+todoList.toggleAll();
 
