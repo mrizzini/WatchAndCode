8dc3031c71879ebbc022a9109a5717db84b2e42d
Matthew Rizzini <matthewrizzini@gmail.com>
2017-11-25 23:18:21 +0000

    Getting date from inputs

--- watch.js
+++ watch.js
@@ -29,13 +29,11 @@ var todoList = {
     this.todos.splice(position, 1);
     this.displayTodos(); 
   },
-  
   toggleCompleted: function (position) {
       var todo = this.todos[position];
       todo.completed = !todo.completed;
       this.displayTodos(); 
   },
-  
   toggleAll: function () {
     var totalTodos = this.todos.length;
     var completedTodos = 0;
@@ -57,15 +55,33 @@ var todoList = {
   }
 };
 
-var displayTodosButton = document.getElementById('displayTodosButton');
-
-displayTodosButton.addEventListener('click', function () {
-  todoList.displayTodos();
-});
-
-
-var toggleAllButton = document.getElementById('toggleAllButton');
-
-toggleAllButton.addEventListener('click', function () {
-  todoList.toggleAll();
-})
+var handlers = {
+  displayTodos: function () {
+    todoList.displayTodos();
+  },
+  addTodo: function () {
+    var addTodoTextInput = document.getElementById('addTodoTextInput');
+    todoList.addTodo(addTodoTextInput.value);
+    addTodoTextInput.value = '';
+  },
+  changeTodo: function () {
+     var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
+     var changeTodoTextInput = document.getElementById('changeTodoTextInput');
+     todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
+     changeTodoPositionInput.value = '';
+     changeTodoTextInput.value = '';
+   },
+  deleteTodo: function () {
+     var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
+     todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
+     deleteTodoPositionInput.value = '';
+   },
+  toggleCompleted: function () {
+     var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
+     todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+     toggleCompletedPositionInput.value = '';
+   },
+  toggleAll: function () {
+      todoList.toggleAll();
+  }
+};
